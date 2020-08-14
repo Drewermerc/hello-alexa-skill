56d9481e75bec5c87f5d4a723b615883c1a4dc74
Drewermerc (FVG) <driver.rx8@gmail.com>
2020-08-14 17:18:39 -0500

    Define an lambda resource for alexa skill

--- src/main/java/com/myorg/HelloAlexaSkillStack.java
+++ src/main/java/com/myorg/HelloAlexaSkillStack.java
@@ -3,6 +3,11 @@ package com.myorg;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.FunctionProps;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.s3.Bucket;
 
 public class HelloAlexaSkillStack extends Stack {
     public HelloAlexaSkillStack(final Construct scope, final String id) {
@@ -12,6 +17,21 @@ public class HelloAlexaSkillStack extends Stack {
     public HelloAlexaSkillStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        // Get source code code from S3 bucket
+        Code sourceCode = Code.fromBucket(
+                Bucket.fromBucketName(this,
+                        "alexa-skill",
+                        "alexa-skill-hello-lambdas"),
+                "HelloAlexaSkillLambda-1.0-jar-with-dependencies.jar"
+        );
+
+        // Lambda properties
+        FunctionProps properties = FunctionProps.builder()
+                .runtime(Runtime.JAVA_8)
+                .code(sourceCode)
+                .handler("com.alexa.skill.HelloWorldStreamHandler::getSkill").build();
+
+        // Define a new Lambda resource
+        final Function alexaskill = new Function(this, "SkillHandler", properties);
     }
 }
